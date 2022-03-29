f65a147f289a30d0f0f4853ed2d73ac9a1762b44
melifarmo <melifarmo@gmail.com>
2022-03-29 20:09:07 +0300

    Made message model

--- boarder_games_django/battleShipsApp/models.py
+++ boarder_games_django/battleShipsApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.views.generic.base import TemplateView, View, RedirectView
 
 # Create your models here.
+User = get_user_model()
+
+
+class Message(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    message = models.CharField(max_length=255)
+    
+# class BattleShipGame(models.Model):
+#     first_player_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='first_player')
+#     second_player_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='second_player')
+#     datetime_of_start = models.DateTimeField(auto_now_add=True)
